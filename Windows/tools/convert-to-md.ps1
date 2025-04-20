3b897a80570663730134ecde056ffb8dbd3a57b4
Matako <82229906+matakko@users.noreply.github.com>
2025-04-20 22:24:47 +0700

    Create convert-to-md.ps1

---
+++ Windows/tools/convert-to-md.ps1
@@ -0,0 +1,9 @@
+$folderPath = "Folder PATH"
+
+
+# Scan folder and give .md at every file because why not ? 
+Get-ChildItem -Path $folderPath -Recurse -File | ForEach-Object {
+    $newName = $_.FullName + ".md"
+    Rename-Item -Path $_.FullName -NewName $newName
+    Write-Host "Renommé : $($_.FullName) -> $newName"
+}
